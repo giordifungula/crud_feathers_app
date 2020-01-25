2db3464ed7d5eb2dd25cc5c26e730b5e9b977fda
giordifungula <jodi.fungula@gmail.com>
2020-01-26 01:09:38 +0200

    test the backend with postman

--- backend/test/services/contact.test.js
+++ backend/test/services/contact.test.js
@@ -4,7 +4,6 @@ const app = require('../../src/app');
 describe('\'contact\' service', () => {
   it('registered the service', () => {
     const service = app.service('contact'); // change contact to contacts
-
     assert.ok(service, 'Registered the service');
   });
 });
