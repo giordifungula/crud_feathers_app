35d959724c8059aa0e2946adabd447d0839aa376
giordifungula <jodi.fungula@gmail.com>
2020-01-26 01:55:12 +0200

    encapsulated the entire app component by the Provider Component

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { Provider } from 'react-redux';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
+import store from "./store"
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>,
+    document.getElementById('root')
 );
--- src/pages/contact-list-page.js
+++ src/pages/contact-list-page.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
+import { connect } from 'react-redux';
 import ContactList from '../components/contact-list';
+import { fetchContacts } from '../actions/contact-actions';
 
 class ContactListPage extends Component {
     render(){
